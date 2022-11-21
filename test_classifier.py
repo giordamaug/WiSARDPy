180ff328de1ae4e8be9a59cea8224fd024dc5d73
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-11-21 15:38:53 +0100

    update

--- test_classifier.py
+++ test_classifier.py
@@ -10,9 +10,26 @@ from lightgbm import LGBMClassifier
 from regression import WiSARDClassifier
 from sklearn.ensemble import RandomForestClassifier
 import utilities as utils
+import time 
+import argparse
+
+parser = argparse.ArgumentParser(description='WiSARD in python')
+parser.add_argument('-i', "--inputfile", dest='inputfile', metavar='<input file>', help='input file name', required=True)
+parser.add_argument('-b', "--nbits", dest='nbits', metavar='<no of bits>', type=int, help='number of bits (default: 4)' , default=4, required=False)
+parser.add_argument('-z', "--ntics", dest='ntics', metavar='<no of tics>', type=int, help='number of tics (default: 128)' , default=128, required=False)
+parser.add_argument('-c', "--cvfold", dest='cvfold', metavar='<no of cv folds>', type=int, help='number of folds' , required=False)
+parser.add_argument('-p', "--jobs", dest='jobs', metavar='<no of parallel jobs>', type=int, help='number of parallel jobs' , required=False)
+parser.add_argument('-s', "--seed", dest='seed', metavar='<seed>', type=int, help='seed (default: 0)' , default=0, required=False)
+parser.add_argument('-m', "--maptype", dest='maptype', metavar='<maptype>', type=str, help='mapping type (default: random, choice: random|linear)', choices=['random', 'linear'], default='random', required=False)
+parser.add_argument('-M', "--method", dest='method', metavar='<method>', type=str, help='classifier name (default: RF, choice: RF|LGBM|WNN)', choices=['RF', 'WNN', 'LGBM'], default='WNN', required=False)
+parser.add_argument('-D', "--debug", action='store_true', required=False)
+parser.add_argument('-S', "--save-embedding", dest='saveembedding',  action='store_true', required=False)
+parser.add_argument('-X', "--display", action='store_true', required=False)
+args = parser.parse_args()
+
 
 # Load the dataset
-df = pd.read_csv('datasets/iris.csv')
+df = pd.read_csv(args.inputfile)
 print(df.head())
 
 labelname = 'species'
@@ -23,7 +40,7 @@ classes = le.classes_
 
 print(f'Datasets dimensions: X={X.shape}, y={tuple(classes)}')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0)
 
 method = 'WNN'
 print(f'Classification with method= "{method}"')
@@ -31,14 +48,24 @@ print(f'Classification with method= "{method}"')
 if method == 'LGBM':
 	clf = LGBMClassifier()
 elif method == 'WNN':
-	clf = WiSARDClassifier(n_bits=16, n_tics=1024, random_state=0, mapping='random', code='t', scale=True, debug=True)
+	clf = WiSARDClassifier(n_bits=args.nbits, n_tics=args.ntics, random_state=args.seed, mapping=args.maptype, code='t', scale=True, debug=args.debug)
 elif method == 'RF':
     clf = RandomForestClassifier()
 
-y_pred = clf.fit(X_train, y_train).predict(X_test)
-#y_pred = cross_val_predict(clf, X, y, cv=10)
+start = time.time()
+if args.cvfold is None:
+	y_pred = clf.fit(X_train, y_train).predict(X_test)
+	targets = y_test
+else:
+	y_pred = cross_val_predict(clf, X, y, cv=args.cvfold)
+	targets = y
+print("--- %s seconds ---" % (time.time() - start))
+print(f'{clf}')
 
-print(f"Classification report: {metrics.classification_report(y, y_pred, target_names=classes)}")
-plt.figure()
-utils.plot_confusion_matrix(metrics.confusion_matrix(y, y_pred), classes=classes,title='Confusion matrix')
-plt.show()
+print(f"Classification report: {metrics.classification_report(targets, y_pred, target_names=classes)}")
+cm = metrics.confusion_matrix(targets, y_pred)
+print(cm)
+if args.display:
+	plt.figure()
+	utils.plot_confusion_matrix(cm, classes=classes,title='Confusion matrix')
+	plt.show()
